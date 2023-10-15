ce98f027ae9c7a42e6b241720490da36074fc8d5
Dilip Kumar <91682425+26dilip@users.noreply.github.com>
2023-10-15 17:47:22 +0530

    Task 3
    Temperature converter web page

---
+++ Task-3/tem.js
@@ -0,0 +1,32 @@
+document.getElementById('convert').onclick = tempConvert;
+document.getElementById('clear').onclick = clearForm;
+
+function tempConvert() {
+    var fahrenheit = document.getElementById ("fahrenheit").value;
+    var celsius = document.getElementById ("celsius").value;
+    var kelvin = document.getElementById ("kelvin").value;
+
+
+    if (fahrenheit != ''){
+        celsius = (parseFloat(fahrenheit) -32) /1.8;
+        kelvin = (parseFloat(fahrenheit) -32) /1.8 + 273.15;
+    }
+    else if(celsius != ''){
+        fahrenheit = (parseFloat(celsius) * 1.8) +32;
+        kelvin = (parseFloat(celsius) + 273.15);
+    }
+    else {
+        fahrenheit = (parseFloat(kelvin) - 273.15) * 1.8 + 32;
+        celsius = (parseFloat(kelvin) - 273.15);
+    }
+
+    document.getElementById('fahrenheit').value = parseFloat(fahrenheit).toFixed(2);
+    document.getElementById('celsius').value = parseFloat(celsius).toFixed(2);
+    document.getElementById('kelvin').value = parseFloat(kelvin).toFixed(2);
+}
+
+function clearForm() {
+    document.getElementById('fahrenheit').value = '';
+    document.getElementById('celsius').value = '';
+    document.getElementById('kelvin').value = '';
+}
